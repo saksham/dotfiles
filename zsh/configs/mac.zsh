d998565e7bded327accf401e18d01648f8628838
Saksham Gautam <saksham.gautam@gmail.com>
2016-10-26 19:53:38 +0200

    Added mac specific settings

---
+++ zsh/configs/mac.zsh
@@ -0,0 +1,3 @@
+# Inpired from https://justin.abrah.ms/dotfiles/zsh.html#sec-2-7
+alias show_hidden="defaults write com.apple.Finder AppleShowAllFiles YES && killall Finder"
+alias hide_hidden="defaults write com.apple.Finder AppleShowAllFiles NO && killall Finder"
